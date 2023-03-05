597fe7adb4aa099c06a3e64da4535fe7db525a10
wyh <wanyuhui1994@gmail.com>
2023-03-05 17:09:12 +0800

    feat: support gpt 3.5 && newest response model

--- Sources/OpenAISwift/Models/OpenAI.swift
+++ Sources/OpenAISwift/Models/OpenAI.swift
@@ -5,11 +5,27 @@
 import Foundation
 
 public struct OpenAI: Codable {
-    public let object: String
+    public let id: String?
+    public let object: String?
     public let model: String?
-    public let choices: [Choice]
+    public let created: Int64?
+    public let choices: [Choice]?
+    public let usage: Usage?
 }
 
 public struct Choice: Codable {
-    public let text: String
+    public let message: Message?
+    public let index: Int?
+    public let finish_reason: String?
+}
+
+public struct Message: Codable {
+    public let role: String?
+    public let content: String?
+}
+
+public struct Usage: Codable {
+    public let prompt_tokens: String?
+    public let completion_tokens: String?
+    public let total_tokens: String?
 }
